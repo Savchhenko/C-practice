679fa1abcc4e5163f28b61a9c936e4d70a621058
Alexandra Savchenko <svchhh18@gmail.com>
2019-06-12 17:50:43 +0300

    The main function is indicated

--- File_i_o_calculator/src/File_i_o_calculator.c
+++ File_i_o_calculator/src/File_i_o_calculator.c
@@ -12,23 +12,36 @@
 #include <stdlib.h>
 
 int main(void) {
-	FILE *fin, *fout; //указатели для обоих файлов
-	fin = fonpen("content.txt","r");
-	if (fin == NULL)
-	    {
-	         puts("Input file cannot be opened");
-	    }
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	FILE *fin, *fout;
+	char c, one, two, v;
+	int size;
+	int *A, *B;
+
+	fin = fopen("content.txt", "r");
 	fout = fopen("output.txt", "w");
-	if (fout == NULL)
-	    {
-	       puts("Output file cannot be opened");
-	    }
+
+	fscanf(fin, "%c", &c);
+	one = c;
+	fscanf(fin, "%c %c", &c);
+	two = c;
+
+	if (two == v) {
+		fscanf(fin, "%c %c %c", &c);
+		size = c;
+		if (size != 0) {
+			A = calloc(size, sizeof(A)); //выделение памяти под вектор
+			B = calloc(size, sizeof(B));
+		}
+
+	}
 
 
-
-
-
-
+	fclose(fin);
+	fclose(fout);
 
 	return EXIT_SUCCESS;
 }
