0fde9b0ec084611f2605398de78211968e934351
Alexandra Savchenko <svchhh18@gmail.com>
2019-07-01 10:33:58 +0300

    околофинальная версия

--- File_i_o_calculator/src/File_i_o_calculator.c
+++ File_i_o_calculator/src/File_i_o_calculator.c
@@ -2,34 +2,36 @@
  ============================================================================
  Name        : File_i_o_calculator.c
  Author      : Bear
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : This work belongs to the student Savchenko Alexandra.
+ Description : Это программа,которая считывает из внешнего файла данные,которые определяют тип калькулятора
+               (калькулятор с обычными числами или векторный калькулятор), операцию и численные значения,с которыми будет проводиться операция.
+               Результат вычислений выводится в другой внешний файл.
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //функция с векторными операциями
 void vector_calc(FILE *fin, FILE *fout, char first) {
 	float *A, *B;
 	float answer;
 	int size; //размерность векторов
-	fscanf(fin, "%i", &size); //прочли символ = размерерность векторов
-	A = malloc(size *sizeof(float)); //выделение памяти под векторы
-    B = malloc(size *sizeof(float));
-    for (int i=0; i<size; i++)
-        fscanf(fin,"%f", &A[i]);
-    for (int k=0; k<size; k++)
-        fscanf(fin,"%f", &B[k]);
+	fscanf(fin, "%i", &size); //прочли символ - размерность векторов
+	A = calloc(size, sizeof(float)); //выделение память под векторы
+    B = calloc(size, sizeof(float));
+    for (int i=0; i<size; i++) //считываем координаты вектора А
+        fscanf(fin, "%f", &A[i]);
+    for (int k=0; k<size; k++) //считываем координаты вектора В
+        fscanf(fin, "%f", &B[k]);
 
     fscanf(fin,"%c",&first);
 	switch (first){
 		case '+':
 			fprintf(fout, "(");
 			for (int i = 0; i < size; i++){
-				fprintf(fin," %.2f ", A[i]);
+				fprintf(fout," %.2f ", A[i]);
 			}
 			fprintf(fout, ") + (");
 			for (int i = 0; i < size; i++){
@@ -46,11 +48,39 @@ void vector_calc(FILE *fin, FILE *fout, char first) {
 			free(B);
 			break;
 		case '-':
+			fprintf(fout, "(");
+			for (int i = 0; i < size; i++){
+				fprintf(fout," %.2f ", A[i]);
+			}
+			fprintf(fout, ") - (");
+			for (int i = 0; i < size; i++){
+				fprintf(fout," %.2f ", B[i]);
+			}
+			fprintf(fout, ") = (");
+			for (int i = 0; i < size; i++) {
+				answer = A[i] - B[i];
+				fprintf(fout," %.2f ", answer);
+			}
+			fprintf(fout, ")");
 
 			free(A);
 			free(B);
 			break;
 		case '*':
+			fprintf(fout, " ( ");
+			for (int i = 0; i < size; i++){
+				fprintf(fout," %.2f ", A[i]);
+			}
+			fprintf(fout, " ) * (");
+			for (int i = 0; i < size; i++){
+				fprintf(fout," %.2f ", B[i]);
+			}
+			fprintf(fout, " ) = ");
+			answer = 0;
+			for (int i = 0; i < size; i++) {
+				answer = answer + (A[i] * B[i]);
+			}
+			fprintf(fout," %.2f", answer);
 
 			free(A);
 			free(B);
@@ -63,9 +93,9 @@ void simple_calc(FILE *fin, FILE *fout, char first) {
 	float *A, *B;
 	float answer;
 	A = calloc(1, sizeof(float));
-	 fscanf(fin, "%f", A);
+	 fscanf(fin, "%f ", A);
 	B = calloc(1, sizeof(float));
-	 fscanf(fout, "%f", B);
+	 fscanf(fout, "%f ", B);
 
 	switch(first){
 	case '+':
@@ -119,28 +149,24 @@ int main(void) {
 	setvbuf(stderr, NULL, _IONBF, 0);
 
 	FILE *fin, *fout; //указатели для обоих файлов
-	char first, second; // переменные, отвечающие за первый и второй символ в файле
-	fin = fopen("content.txt","r");
+	char first, second; // переменные, отвечающие за первый и второй символ в файле(операция и тип калькулятора)
+	fin = fopen("content.txt", "r");
 	if (fin == NULL)
 	    {
-	         puts("Input file cannot be opened");
+	         puts("Input file cannot be opened.");
 	    }
 	fout = fopen("output.txt", "w");
-	if (fout == NULL)
-	    {
-	       puts("Output file cannot be opened");
-	    }
-
-	fscanf(fout,"%c %c", &first, &second); //прочли первые два символа и присвоили их переменным
+	fscanf(fin, "%c %c", &first, &second); //прочли первые два символа и присвоили их переменным
+	printf("Type of calculator: %c\n", second);
+	printf("Operation: %c\n", first);
+	//выбор калькулятора:
     if(second == 'v') {
     	vector_calc(fin, fout, first);
     }
-    if(second == 's') {
+    else  {
     	simple_calc(fin, fout, first);
     }
-    else {
-    	printf("Error: invalid value in input file.");
-    }
+    printf("See the result in the external file.");
 
     fclose(fin); //закрыли оба файла
 	fclose(fout);
