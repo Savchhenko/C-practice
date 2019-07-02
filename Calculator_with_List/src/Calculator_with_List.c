f89821f815c1bb555e28152074fbb0086b53e39c
Alexandra Savchenko <svchhh18@gmail.com>
2019-07-02 14:15:13 +0300

    Первая версия

---
+++ Calculator_with_List/src/Calculator_with_List.c
@@ -0,0 +1,168 @@
+/*
+ ============================================================================
+ Name        : Calculator_with_List.c
+ Author      : Bear
+ Copyright   : This work belongs to the student Alexandra Savchenko
+ Description : Программа калькулятора,которая читает данные из внешнего файла и записывает их в список element,после чего файл закрывается,
+               проводятся вычисления, и их результаты заносятся в новый список list_with_result и выводятся в другой внешний файл.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//создаем список(в виде структуры),в котором будут храниться данные
+typedef struct element {
+    char choice; //первый символ в файле,отвечающий за выбор операции
+    char type; //второй символ,отвечающий за тип калькулятора (v/s)
+    int number; //третий символ,отвечающий за размерность векторов (в случае численного калькулятора number=1)
+    float *value1;
+    float *value2;
+    struct element *next;
+} element;
+element *last = NULL;
+element *head = NULL;
+
+//создаем список,в котором будут храниться данные с результатами вычислений
+typedef struct list_with_result {
+    char oper;
+    int size;
+    float *result;
+    struct list_with_result *next;
+} res;
+res *lastres = NULL;
+res *headres = NULL;
+
+//функция,заполняющая список element данными из файла
+void filling( FILE* fin) {
+    element *tmp = (element*)malloc(sizeof(element));
+    fscanf(fin, " %c %c", &tmp->choice, &tmp->type);
+    if (tmp->type == 'v')
+        fscanf(fin, "%d", &tmp->number);
+    else
+        tmp->number = 1;
+    tmp->value1 = malloc(tmp->number*sizeof(float)); //выделили память под координаты первого вектора
+    tmp->value2 = malloc(tmp->number*sizeof(float)); //выделили память под координаты второго вектора
+    for (int i = 0; i < tmp->number; i++)
+        fscanf(fin, "%f", &tmp->value1[i]);
+    if (tmp->choice !='!')
+    {
+        tmp->value2 = malloc(tmp->number*sizeof(float));
+        for (int i = 0; i < tmp->number; i++)
+            fscanf(fin, "%f", &tmp->value2[i]);
+    }
+if  (last != NULL)
+{
+    last->next = tmp;
+    tmp->next = NULL;
+    last = tmp;
+}
+else {
+    head = tmp; //передали head,который был пустой,значение переменной
+    last = tmp; //передали last,который был пустой,значение переменной
+}
+}
+void pushres ()
+{
+    res *tmp_res = (res*)malloc(sizeof(element));
+    tmp_res->result = malloc(last->number*sizeof(float));
+    tmp_res->size = last->number;
+    tmp_res->oper = last->type;
+    switch(last->type)
+    {
+        case 's' :
+        	switch(last->choice)
+			{
+				case '+' :
+					tmp_res->result[0] = last->value1[0] + last->value2[0];
+					break;
+				case '-' :
+					tmp_res->result[0] = last->value1[0] - last->value2[0];
+					break;
+				case '!' :
+					tmp_res->result[0] =1 ;
+					for (int c = 0;c<=last->value1[0];c++)
+					 {
+									 tmp_res->result[0]=tmp_res->result[0]*c;
+					 }
+					break;
+				case '*' :
+					tmp_res->result[0] = last->value1[0] * (last->value2[0]);
+					break;
+				case '/' :
+					tmp_res->result[0] = last->value1[0] / last->value2[0];
+					break;
+				case '^' :
+					tmp_res->result[0] = last->value1[0] ;
+					for (float c = 1 ;c<last->value2[0];c++)
+					{
+						tmp_res->result[0]= tmp_res->result[0] * last->value1[0];
+					}
+					break;
+        }break;
+        case 'v' :switch(last->choice)
+        {
+            case '+':
+                for (int m = 0;m<last->number;m++)
+                {
+                    tmp_res->result[m]= last->value1[m] +last->value2[m];
+                }
+                break;
+           case '-':
+               for (int m = 0;m<last->number;m++)
+               {
+                   tmp_res->result[m]= last->value1[m] - last->value2[m];
+               }
+               break;
+          case '*':
+              for (int m = 0;m<last->number;m++)
+               {
+                  tmp_res->result[m]=last->value1[m] * last->value2[m];
+               }
+              break;
+        }
+    }
+	if  (lastres != NULL)
+	{
+		lastres->next = tmp_res;
+		tmp_res->next = NULL;
+		lastres = tmp_res;
+	}
+	else {
+		headres = tmp_res;
+	  lastres = tmp_res;
+	}
+}
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+
+    FILE *fin;
+    fin = fopen("input.txt","r");
+    while (!feof(fin)) //пока не конец файла
+    {
+        filling( fin);
+    }
+    fclose(fin);
+    last = head;
+    while(last !=NULL)
+    {
+        pushres () ;
+        last =last->next;
+    }
+    lastres = headres;
+    FILE  *fout;
+    fout = fopen("output.txt","w");
+    while(lastres !=NULL)
+    {
+        for (int y =0;y<lastres->size;y++)
+        {
+            fprintf (fout,"%f ",lastres->result[y]);
+        }
+        lastres =lastres->next;
+    }
+        fclose(fout);
+    return EXIT_SUCCESS;
+}
